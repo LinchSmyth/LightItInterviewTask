5058c4d4d3121ed3ec682465dbc22700bddb20a5
LinchSmyth <andrei95-1995@mail.ru>
2016-07-13 18:10:17 +0300

    Add route for encrypt/decrypt method

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/help'
   get 'pages/about'
+  post '/pages/result', :controller => 'pages', :action => 'result'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
